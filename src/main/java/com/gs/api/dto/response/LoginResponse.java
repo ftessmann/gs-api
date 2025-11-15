714cfaf3b092083a62c37561b5e3066c4d208a5d
ftessmann <fernando.tessmann@gmail.com>
2025-11-15 20:42:20 -0300

    feat: response and request dtos

---
+++ src/main/java/com/gs/api/dto/response/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.gs.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String message;
+    private String username;
+    private String token;
+}
