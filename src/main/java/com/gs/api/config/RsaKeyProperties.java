5726c10e83637c578c922f9176c0c14da09e7dac
ftessmann <fernando.tessmann@gmail.com>
2025-11-15 20:43:54 -0300

    feat: key generator

---
+++ src/main/java/com/gs/api/config/RsaKeyProperties.java
@@ -0,0 +1,39 @@
+package com.gs.api.config;
+
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Component
+public class RsaKeyProperties {
+
+    private RSAPublicKey publicKey;
+    private RSAPrivateKey privateKey;
+
+    public RsaKeyProperties() {
+        KeyPair keyPair = generateRsaKey();
+        this.publicKey = (RSAPublicKey) keyPair.getPublic();
+        this.privateKey = (RSAPrivateKey) keyPair.getPrivate();
+    }
+
+    private KeyPair generateRsaKey() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException("Failed to generate RSA key pair", ex);
+        }
+    }
+
+    public RSAPublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public RSAPrivateKey getPrivateKey() {
+        return privateKey;
+    }
+}
