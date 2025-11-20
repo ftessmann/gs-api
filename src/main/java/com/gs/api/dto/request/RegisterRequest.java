2d14cc2fed8f87bc3f3496699e9a86a58f350115
ftessmann <fernando.tessmann@gmail.com>
2025-11-20 19:13:03 -0300

    feat: feat add name and phone to register request

--- src/main/java/com/gs/api/dto/request/RegisterRequest.java
+++ src/main/java/com/gs/api/dto/request/RegisterRequest.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    private String name;
+    private String telephone;
     private String username;
     private String email;
     private String password;
