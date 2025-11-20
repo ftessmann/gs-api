123c810d2e3533aec884b1565ba709847df21754
ftessmann <fernando.tessmann@gmail.com>
2025-11-20 19:12:43 -0300

    feat: open api config

---
+++ src/main/java/com/gs/api/config/OpenApiConfig.java
@@ -0,0 +1,48 @@
+package com.gs.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("OpenAPI Documentation - Development");
+
+        Info info = new Info()
+                .title("GS - API Documentation")
+                .version("0.1.0")
+                .description("Managing environmental data for better job quality");
+
+        SecurityScheme security = new SecurityScheme()
+                .name(SECURITY_SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(SECURITY_SCHEME_NAME);
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(server))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, security)
+                )
+                .addSecurityItem(securityRequirement);
+    }
+}
