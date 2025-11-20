62268796052ceb4267c558bce64e043b59023c05
ftessmann <fernando.tessmann@gmail.com>
2025-11-20 19:29:53 -0300

    feat: mapped webhook json request

--- src/main/java/com/gs/api/dto/request/SensorReadingCreateRequestDTO.java
+++ src/main/java/com/gs/api/dto/request/SensorReadingCreateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.gs.api.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -7,12 +8,24 @@ import lombok.Data;
 public class SensorReadingCreateRequestDTO {
 
     @NotNull
+    @JsonProperty("code")
     private String sensorCode;
 
+    @JsonProperty("temperatura")
     private Double temperature;
+
+    @JsonProperty("umidade")
     private Double humidity;
+
+    @JsonProperty("co")
     private Double co;
+
+    @JsonProperty("co2")
     private Double co2;
+
+    @JsonProperty("ch4")
     private Double ch4;
+
+    @JsonProperty("h2s")
     private Double h2s;
 }
